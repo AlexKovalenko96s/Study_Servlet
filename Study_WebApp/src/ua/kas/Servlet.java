d1a9922d6049b6c4c279643d74889b7cbfc05a5d
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-10-13 21:11:09 +0300

    web.xml and/or annotations

--- Study_WebApp/src/ua/kas/Servlet.java
+++ Study_WebApp/src/ua/kas/Servlet.java
@@ -4,20 +4,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Servlet")
+//@WebServlet(value = "/Servlet", description = "Мое описание сервлета", displayName = "displayName")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("info");
+		System.out.println("Servlet.goGet()");
 
 		// response.setContentType("text/html");
 		// response.setCharacterEncoding("UTF-8");
+		// or
 
 		response.setContentType("text/html; charset=utf-8");
 
