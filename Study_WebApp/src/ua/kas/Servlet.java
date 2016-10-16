2bcda548e7f00f4c739f8e9ea342ad6f2b6e717e
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-10-16 21:01:44 +0300

    //

--- Study_WebApp/src/ua/kas/Servlet.java
+++ Study_WebApp/src/ua/kas/Servlet.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet(value = "/Servlet", description = "Мое описание сервлета", displayName = "displayName")
+@WebServlet(value = "/Servlet", description = "Мое описание сервлета", displayName = "displayName")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -36,11 +37,11 @@ public class Servlet extends HttpServlet {
 		String name = request.getParameter("firstName");
 		String surname = request.getParameter("secondName");
 
-		// response.setContentType("text/html");
-		// response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
 		// or
 
-		response.setContentType("text/html; charset=utf-8");
+		// response.setContentType("text/html; charset=utf-8");
 
 		PrintWriter out = response.getWriter();
 		out.print("<h3>Hello from post" + " " + name + " " + surname + "</h3><br>Привіт!");
